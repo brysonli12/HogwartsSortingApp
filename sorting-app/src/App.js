00b87a0e9232797542ff01d0f6cc84609fa20a52
Bryson Li <brysonli@gmail.com>
2019-02-16 22:21:02 -0800

    Start app Begin development of app

---
+++ sorting-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gryffindor: 0,
+    	ravenclaw: 0,
+    	slytherin: 0,
+    	hufflepull: 0,
+    	currentQuestion: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
