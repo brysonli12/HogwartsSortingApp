24aadd2909be41fd66e0376bac657890c44cab13
Matthew Hu <matthu24@gmail.com>
2019-03-01 23:54:03 -0800

    Hello Bryson

--- sorting-app/src/questions/Introduction.js
+++ sorting-app/src/questions/Introduction.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Introduction = () => {
-  return(
-    <div>
-      hello welcome to sorting app.
-    </div>
-  )
-}
+  return <div>hello welcome to sorting app. Hello Bryson.</div>;
+};
 
 export default Introduction;
