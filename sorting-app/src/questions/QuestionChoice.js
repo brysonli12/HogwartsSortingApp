9a569e899d987e5253b9dfb4a7f213fae2639149
Bryson Li <brysonli@gmail.com>
2019-04-09 16:39:04 -0700

    First introduction of new QuestionChoice with QuestionForm
    also working on getting formatting working

--- sorting-app/src/questions/QuestionChoice.js
+++ sorting-app/src/questions/QuestionChoice.js
@@ -1,56 +1,26 @@
 import React, { Component } from 'react';
-// import { Form, Header, Button } from 'semantic-ui-react';
 
 class QuestionChoice extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      selected: this.props.choice.selected,
-      text: this.props.choice.choice_text,
+      selected: false
     }
-
-    this.handleClick = this.handleClick.bind(this);
   }
+
   componentDidUpdate() {
-    console.log(this.state);
+    // console.log(this.state);
   }
-  /*handleChange(e, { value }) {
-    this.setState({ selectedAnswer: value })
-  }*/
 
-  // use a callback function here maybe to allow for QuestionForm to get updated
-  // details on which question was pressed (to update)
-  // It's probably a good idea for QuestionForm to have the state of the choices
-  // which are passed to components (to simplify handleClick logic)
-  // but each QuestionChoice will have a QuestionOrder# or House# or something else
-  //      to identify the unique choice
   handleClick(){
-    console.log('choice was clicked');
-    this.setState({ selected: !this.state.selected});
+    this.props.handleClick(this.props.value);
   }
 
-  renderChoice() {
-    if (this.state.selected) {
-
-    return <div className="qchoice qselected" onClick={this.handleClick}>
-      {this.state.text}
-
-      </div>
-    }
-    else {
-
-        return <div className="qchoice" onClick={this.handleClick}>
-          {this.state.text}
-
-      </div>
-    }
-  }
 
   render() {
     return (
-      <div>
-        {this.renderChoice()}
+      <div className="qchoice" onClick={this.handleClick.bind(this)}>
+          <div className={this.props.selected ? "qchoice selected" : "qchoice"} data-value={this.props.value}>{this.props.label}</div>
       </div>
     )
   }
--- sorting-app/src/questions/QuestionForm.js
+++ sorting-app/src/questions/QuestionForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Form, Header, Button } from 'semantic-ui-react';
-// import QuestionChoice from './questions/QuestionChoice'
+import { Header, Button } from 'semantic-ui-react';
+import QuestionChoice from './QuestionChoice'
 /*
-
+Form
 <QuestionChoice choice={ {choice_text: "A choice test", selected: false }}/>
 */
 
@@ -14,14 +14,15 @@ class QuestionForm extends Component {
       selectedAnswer: null,
     }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleNextQuestion = this.handleNextQuestion.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleNextQuestion = this.handleNextQuestion.bind(this);
+    this.handleChoice = this.handleChoice.bind(this);
   }
   componentDidUpdate() {
     console.log(this.state);
   }
-  handleChange(e, { value }) {
-
+  handleChange(value) { //e, { value }) {
+    //console.log(e);
     this.setState({ selectedAnswer: value })
   }
   handleNextQuestion() {
@@ -29,7 +30,10 @@ class QuestionForm extends Component {
     this.setState( {selectedAnswer: null} );
     this.props.handleNext();
   }
-
+  handleChoice(e) {
+    console.log('handling choice');
+    console.log(e);
+  }
   render() {
     let question, sly, rav, huf, gry, order;
     ( { question, sly, rav, huf, gry, order }  = this.props.question );
@@ -39,49 +43,36 @@ class QuestionForm extends Component {
         <Header style={{position: 'fixed', top: '25%',left: '5%', width: '30%', fontSize: '50'}}>
           Question: {question}
         </Header>
-
-        <Form  style={{position: 'fixed', top: '25%',right: '5%', width: '30%', fontSize: '50'}}>
-          <Form.Group>
-            <Form.Radio
-              toggle
-              label={questions[order[0]][1]}
-              name='radioGroup'
-              value={questions[order[0]][0]}
-              checked={this.state.selectedAnswer === questions[order[0]][0]}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Radio
-              toggle
-              label={questions[order[1]][1]}
-              name='radioGroup'
-              value={questions[order[1]][0]}
-              checked={this.state.selectedAnswer === questions[order[1]][0]}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Radio
-              toggle
+        <div style={{position: 'fixed', top: '0px',right: '0px', width: '30%', fontSize: '50'}}>
+          <QuestionChoice
+            label={questions[order[0]][1]}
+            name='radioGroup'
+            value={questions[order[0]][0]}
+            selected={this.state.selectedAnswer === questions[order[0]][0]}
+            handleClick={this.handleChange}
+          />
+        <QuestionChoice
+            label={questions[order[1]][1]}
+            name='radioGroup'
+            value={questions[order[1]][0]}
+            selected={this.state.selectedAnswer === questions[order[1]][0]}
+            handleClick={this.handleChange}
+          />
+          <QuestionChoice
               label={questions[order[2]][1]}
               name='radioGroup'
               value={questions[order[2]][0]}
               checked={this.state.selectedAnswer === questions[order[2]][0]}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Radio
-              toggle
+              handleClick={this.handleChange}
+          />
+          <QuestionChoice
               label={questions[order[3]][1]}
               name='radioGroup'
               value={questions[order[3]][0]}
-              checked={this.state.selectedAnswer === questions[order[3]][0]}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-        </Form>
+              selected={this.state.selectedAnswer === questions[order[3]][0]}
+              handleClick={this.handleChange}
+          />
+        </div>
         <Button onClick={this.handleNextQuestion}>
           Next
         </Button>
